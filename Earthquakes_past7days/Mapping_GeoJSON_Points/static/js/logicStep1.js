151920cfa32c61f641acf9cbf9271f435a43c024
bcruz96 <bcruz96@outlook.com>
2022-11-19 02:37:48 -0800

    Mapping earthquakes on a simple map.

--- Earthquakes_past7days/Mapping_GeoJSON_Points/static/js/logicStep1.js
+++ Earthquakes_past7days/Mapping_GeoJSON_Points/static/js/logicStep1.js
@@ -6,7 +6,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/t
 });
 
 // We create the dark view tile layer that will be an option for our map.
-let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -14,31 +14,23 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 
 // Create a base layer that holds both maps.
 let baseMaps = {
-    Street: streets,
-    Dark: dark
+    "Streets": streets,
+    "Satellite": satelliteStreets
   };
 
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
-    center:[30, 30],
-    zoom: 2,
+    center:[39.5, -98.5],
+    zoom: 3,
     layer: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Add GeoJSON data.
-let airportData = "https://raw.githubusercontent.com/brandoncruzGU/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json";
-
-// Grabbing our GeoJSON data.
-d3.json(airportData).then(function(data) {
-    console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data, {
-        onEachFeature: function (f, l) {
-            l.bindPopup("<h2> Airport code: " + f.properties.faa + "</h2> <hr> <h3> Airport name: " + f.properties.name + "</h3>");
-        }
-    }).addTo(map);
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
 
